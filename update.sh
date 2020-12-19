9d8fd3a8480ef07ea268cf443dc315b5e1ffa37a
abdel-hakim <hakim.linux@gmail.com>
2020-12-19 19:05:50 +0200

    first one

---
+++ update.sh
@@ -0,0 +1,3 @@
+#Run by this command :
+# ./update.sh ourdemoinfra ourinfra.yml ourinfra-params.json
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
